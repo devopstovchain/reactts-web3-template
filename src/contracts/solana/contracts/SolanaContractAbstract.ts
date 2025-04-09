a2f131ee99cef71fa2ae190e365d06a920139450
Le Dinh Quyen <quyenld9699@gmail.com>
2025-04-09 10:22:35 +0700

    update contract solana abstract

--- src/contracts/solana/contracts/SolanaContractAbstract.ts
+++ src/contracts/solana/contracts/SolanaContractAbstract.ts
@@ -1,27 +1,43 @@
 import { AnchorProvider, Idl, Program } from '@project-serum/anchor';
-import { Wallet } from '@project-serum/anchor/dist/cjs/provider';
-import { ComputeBudgetProgram, PublicKey, Transaction } from '@solana/web3.js';
+import { SendTransactionOptions } from '@solana/wallet-adapter-base';
+import { WalletContextState } from '@solana/wallet-adapter-react';
+
+import { ComputeBudgetProgram, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js';
 import { publicClientSol } from 'src/states/wallets/solana-blockchain/configs';
+import { sleep } from 'src/utils';
 
 export abstract class SolanaContractAbstract<IDL extends Idl> {
     public provider: AnchorProvider;
     public program: Program<IDL>;
+    public walletContext: WalletContextState;
 
-    constructor(wallet: Wallet, programId: PublicKey, idl: IDL) {
-        this.provider = new AnchorProvider(publicClientSol, wallet, { preflightCommitment: 'confirmed' });
+    constructor(walletContext: WalletContextState, programId: PublicKey, idl: IDL) {
+        this.provider = new AnchorProvider(publicClientSol, walletContext as any, { preflightCommitment: 'confirmed' });
+        this.walletContext = walletContext;
         this.program = new Program(idl, programId, this.provider);
     }
-
-    abstract initialize(...args: any[]): Promise<string>;
 
     setfeeGas(gas: number) {
         return ComputeBudgetProgram.setComputeUnitPrice({
             microLamports: gas,
         });
     }
-    async sendTransaction(instruction: Transaction, signers: any[] = []) {
-        const transaction = new Transaction().add(instruction);
-        const signature = await this.provider.sendAndConfirm(transaction, signers, { maxRetries: 1000 * 60 });
+
+    async awaitConfirmTransaction(signature: string) {
+        const latestBlockHash = await publicClientSol.getLatestBlockhash();
+
+        await publicClientSol.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: signature,
+        });
+        await sleep(600);
+    }
+
+    async sendTransaction(transaction: Transaction | VersionedTransaction, options?: SendTransactionOptions) {
+        const signature = await this.walletContext.sendTransaction(transaction, publicClientSol, { maxRetries: 1000 * 60, preflightCommitment: 'finalized', ...options });
+        await this.awaitConfirmTransaction(signature);
+
         return signature;
     }
 }
