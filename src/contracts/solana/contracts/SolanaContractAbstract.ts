e1f0251dbe314715cd0e8d382595d8d54e337474
Le Dinh Quyen <quyenld9699@gmail.com>
2025-04-15 11:43:23 +0700

    update template

--- src/contracts/solana/contracts/SolanaContractAbstract.ts
+++ src/contracts/solana/contracts/SolanaContractAbstract.ts
@@ -12,9 +12,10 @@ export abstract class SolanaContractAbstract<IDL extends Idl> {
     public walletContext: WalletContextState;
 
     constructor(walletContext: WalletContextState, programId: PublicKey, idl: IDL) {
-        this.provider = new AnchorProvider(publicClientSol, walletContext as any, { preflightCommitment: 'confirmed' });
+        this.provider = new AnchorProvider(publicClientSol, walletContext as any, { preflightCommitment: 'finalized' });
         this.walletContext = walletContext;
         this.program = new Program(idl, programId, this.provider);
+        this.walletContext = walletContext;
     }
 
     setfeeGas(gas: number) {
@@ -35,6 +36,29 @@ export abstract class SolanaContractAbstract<IDL extends Idl> {
     }
 
     async sendTransaction(transaction: Transaction | VersionedTransaction, options?: SendTransactionOptions) {
+        if (transaction instanceof VersionedTransaction) {
+            const simulate = await publicClientSol.simulateTransaction(transaction);
+            console.log('Simulation:', simulate);
+            if (simulate.value.err) {
+                throw new Error('Simulate error: ' + simulate.value.err.toString());
+            }
+        }
+        if (this.walletContext.wallet?.adapter.name === 'Phantom') {
+            console.log('Phantom wallet detected');
+            const provider = window?.phantom?.solana;
+            if (transaction instanceof Transaction) {
+                if (!this.walletContext.publicKey) {
+                    throw new Error('Wallet not connected!');
+                }
+                transaction.feePayer = this.walletContext.publicKey;
+                transaction.recentBlockhash = (await publicClientSol.getLatestBlockhash()).blockhash;
+            }
+            const { signature } = await provider?.signAndSendTransaction(transaction);
+            console.log('Signature:', signature);
+            await this.awaitConfirmTransaction(signature);
+            return signature;
+        }
+
         const signature = await this.walletContext.sendTransaction(transaction, publicClientSol, { maxRetries: 1000 * 60, preflightCommitment: 'finalized', ...options });
         await this.awaitConfirmTransaction(signature);
 
